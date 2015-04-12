5b3bf4634a0d3d0e8b8d9953f21c1b067e4e80fc
tomaszpanek94@gmail.com <>
2015-04-12 10:24:00 +0200

    Zmiana pliku index.html na index.php

--- Aplikacja/index.php
+++ Aplikacja/index.php
@@ -3,6 +3,7 @@
 echo "Hello world";
 echo "Netbeans commit";
 echo "Test commit 2";
+echo "Test commit 3";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
