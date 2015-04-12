f6c2562204186c7312b5eeb961f89be2b8fa2029
noinek <noinek@noinek-PC>
2015-04-12 10:06:31 +0200

    Zmiana pliku index.html na index.php

---
+++ Aplikacja/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Hello world";
+echo "Netbeans commit";
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
