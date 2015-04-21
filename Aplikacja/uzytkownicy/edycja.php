d87c01cc5e73195330009c2324ec4749e782944c
damian12345 <d.pankow@o2.pl>
2015-04-21 09:33:10 +0200

    Edycja użytkowników. Współpraca z Tomkiem. Tylko imię i nazwisko.

---
+++ Aplikacja/uzytkownicy/edycja.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include "connect.php";
+mysql_connect("$host","$db_user","$db_password");
+mysql_select_db("$db_name");
+?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ankiety Online</title>
+	</head>
+        <body>
+            <form method="POST" action="edycja2.php">
+                 Imie: <br/> <input type="text" name="imie"/><br/>
+                 Nazwisko: <br/> <input type="text" name="nazwisko"/><br/>
+                 <input type="submit" value="Zmień">
+            </form>
+        </body>
+</html>
